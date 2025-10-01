07912de83dda5d6797e42756ab1d54234e3e0220
qurrahazam <qurrahazam@gmail.com>
2025-10-01 16:16:20 +0500

    ADDED: mongodb connection

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+// Following line avoid multiple connection issue in development.
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { connection: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.connection) return cached.connection;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  }
+  cached.connection = await cached.promise;
+  return cached.connection;
+}
