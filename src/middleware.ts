116d7d40b5787bd79304d82484e04ccd9e538c08
qurrahazam <qurrahazam@gmail.com>
2025-10-02 12:31:49 +0500

    UPDATED: block direct access to change-password without login

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,20 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
 
-  // If logged in, block access to /login or /signup
   if (token && (req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/signup")) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
-
-  // If not logged in, block access to /dashboard
   if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
+  if (!token && req.nextUrl.pathname.startsWith("/change-password")) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/login", "/signup", "/dashboard/:path*"],
+  matcher: ["/login", "/signup", "/change-password", "/dashboard/:path*"],
 };
