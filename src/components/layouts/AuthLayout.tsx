4b5eb1f509ebb6f1fe7f76c61000af6b23272cbe
qurrahazam <qurrahazam@gmail.com>
2025-10-06 18:46:54 +0500

    ADDED: resuable ui component for bg and title

---
+++ src/components/layouts/AuthLayout.tsx
@@ -0,0 +1,26 @@
+"Use Client";
+import { Inter } from "next/font/google";
+import { title } from "process";
+import React, { ReactNode } from "react";
+import { Interface } from "readline";
+
+
+interface AuthLayoutProps {
+  title?: string;
+  subtitle?: string;
+  children: ReactNode;
+}
+
+export default function AuthLayout({children, title, subtitle }: AuthLayoutProps) {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-50 to-emerald-100">
+        <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8"> 
+            <h1 className="text-3xl font-bold text-center text-emerald-600 mb-2">
+          {title || "Welcome"}
+        </h1>
+        <p className="text-center text-gray-600 mb-4">{subtitle || "Please enter your details below."}</p>
+       {children}
+        </div>
+        </div>
+    );
+    }
