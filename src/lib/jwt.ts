90c6ee34b788abfa03005d427a652dbd17070bd3
qurrahazam <qurrahazam@gmail.com>
2025-10-13 17:51:13 +0500

    ADDED: token validator and generator

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -2,13 +2,13 @@ import jwt, { Secret } from "jsonwebtoken";
 
 const SECRET: Secret = process.env.JWT_SECRET || "HarryPotter";
 
-export function generateToken(payload: object, expiresIn?: string | number) {
+export function generateToken(payload: object, expiresIn?: string) {
   return jwt.sign(payload, SECRET, { expiresIn: expiresIn ?? "1h" });
 }
 
 export function verifyToken(token: string) {
   try {
-    return jwt.verify(token, SECRET);
+    return jwt.verify(token, SECRET) as { id: string; email: string };
   } catch {
     return null;
   }
