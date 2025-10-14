d735f83cc269b3f4ed7f428f22ab4471ca95ee99
qurrahazam <qurrahazam@gmail.com>
2025-10-14 16:11:40 +0500

    ADDED: schema to save posts

---
+++ src/models/Post.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const Post = new mongoose.Schema({
+  title: { type: String, required: true },
+  slug: { type: String, unique: true, required: true },
+  content: { type: String, required: true },
+  author: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.models.Post || mongoose.model("Post", Post);
