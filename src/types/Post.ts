c7b17c84036cb8ca4c464a977457c796857da2cf
qurrahazam <qurrahazam@gmail.com>
2025-10-16 17:50:05 +0500

    ADDED: post interface

---
+++ src/types/Post.ts
@@ -0,0 +1,19 @@
+export interface Post {
+  _id: string;
+  title: string;
+  slug: string;
+  content: string;
+  excerpt?: string;
+  coverImage?: string;
+  tags?: string[];
+  category?: string;
+  author?: {
+    name?: string;
+    email?: string;
+  };
+  views?: number;
+  likes?: number;
+  readTime?: number;
+  isPublished?: boolean;
+  createdAt: Date;
+}
