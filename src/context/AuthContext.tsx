2c2651cda38286af7d7f620848ca41aa7375f82d
qurrahazam <qurrahazam@gmail.com>
2025-10-16 17:53:32 +0500

    ADDED: context to inform to the whole system about user login

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface AuthContextType {
+  user: any;
+  setUser: (user: any) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        if (!res.ok) return;
+        const data = await res.json();
+        setUser(data.user);
+      } catch (err) {
+        console.error("Auth check failed", err);
+      }
+    };
+
+    checkUser();
+  }, []);
+
+
+  const logout = async () => {
+    await fetch("/api/auth/logout", { method: "POST" });
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used inside AuthProvider");
+  return context;
+};
